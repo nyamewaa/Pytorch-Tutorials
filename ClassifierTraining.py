1c87f7705e066ced0541ccc9506e073aa7bcff20
Mercy Asiedu <mercy.asiedu@duke.edu>
2019-09-11 15:27:12 -0400

    training the network

--- ClassifierTraining.py
+++ ClassifierTraining.py
@@ -81,5 +81,30 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
 
 #%% TRAIN THE NETWORK
-
+for epoch in range(2): #loop over data set multiple times
+    
+     running loss = 0.0
+     for i, data in enumerate(trainloader, 0):
+         # gets the inputs; data is a list of [inputs, labels]
+         inputs, labels = data
+         
+         #zero the parameter gradients
+         optimizer.zero_grad()
+         
+         # forward + backward + optimize
+         outputs = net(inputs)
+         loss = criterion(output, labels)
+         loss.backward()
+         optimizer.step()
+         
+         #print statistics
+         running_loss += loss.item()
+         if i % 2000 == 1999: #print every 2000 mini-batches
+             print('[%d, %5d] loss: %.3f' %
+                   (epoch +1, i + 1, running_loss / 2000))
+             running_loss=0.0
+             
+print ('Finished Training')
+             
+         
      
