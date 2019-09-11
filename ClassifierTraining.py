3f50e7237f8b6957e22d039ddb74259544188150
Mercy Asiedu <mercy.asiedu@duke.edu>
2019-09-11 16:04:33 -0400

    complete

--- ClassifierTraining.py
+++ ClassifierTraining.py
@@ -83,7 +83,7 @@ optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
 #%% TRAIN THE NETWORK
 for epoch in range(2): #loop over data set multiple times
     
-     running loss = 0.0
+     running_loss = 0.0
      for i, data in enumerate(trainloader, 0):
          # gets the inputs; data is a list of [inputs, labels]
          inputs, labels = data
@@ -93,7 +93,7 @@ for epoch in range(2): #loop over data set multiple times
          
          # forward + backward + optimize
          outputs = net(inputs)
-         loss = criterion(output, labels)
+         loss = criterion(outputs, labels)
          loss.backward()
          optimizer.step()
          
@@ -105,6 +105,55 @@ for epoch in range(2): #loop over data set multiple times
              running_loss=0.0
              
 print ('Finished Training')
-             
-         
+
+#%% TEST THE NETWORK ON TRAINING DATA
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+#print images and test for a subset of images n=4
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ''.join('%5s' %  classes[labels[j]] for j in range(4)))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+#test for the entire data set
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images,labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+#%% Which classes performed well and which didn't??
+class_correct = list(0. for i in  range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+            
+for i in range(10):
+    print('Accuracy of %5s: %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+    
+
+
+ 
      
+     
+   
